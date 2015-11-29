4d523764869177da412a55d346a8a0c36daa519f
terakilobyte <terakilobyte@gmail.com>
2015-11-29 09:22:32 -0800

    Update tests and reporter

--- tests/reducers.spec.js
+++ tests/reducers.spec.js
@@ -15,38 +15,39 @@ const setup = () => {
 
 };
 
-test('#reducer.spec', (t) => {
+test('reducer.spec', (t) => {
 
-  t.test('Test 1 should return a default state if none is provided', (t) => {
+  t.test('reducer should return a default state if none is provided', (t) => {
     let fixtures = setup();
     const undefinedState = undefined;
     const actual = reducer(undefinedState, fixtures._action);
     const expected = fixtures._state;
 
-    // t.plan() says how many assertions we're going to run for this test
-    t.plan(1);
-    t.deepEqual(actual, expected);
+    t.deepEqual(actual, expected, 'reducer returns a default state');
+    t.end();
   });
 
-  t.test('Test 2 should return a new state upon a valid action type', (t) => {
+  t.test('reducer should return a new state upon a valid action type', (t) => {
     const fixtures = setup();
     let action = fixtures._action;
     action.type = 'backAction';
     const actual = reducer(fixtures._state, action);
     const expected = fixtures._state;
 
-    t.plan(1);
-    t.notEqual(actual, expected);
+    t.notEqual(actual, expected,
+      'reducer returns a new state if action is valid');
+    t.end();
   });
 
-  t.test('Test 3 should return previous state on invalid action', (t) => {
+  t.test('reducer should return previous state on invalid action', (t) => {
     const fixtures = setup();
     const action = {type: 'nonsense'};
     const actual = reducer(expected, action);
     const expected = fixtures._state;
-    t.plan(1);
-    t.deepEqual(actual, expected);
+
+    t.deepEqual(actual, expected,
+      'reducer returns previous state if the action is invalid');
+    t.end();
   });
   t.end();
 });
-
--- tests/test.spec.js
+++ tests/test.spec.js
@@ -1,24 +1,24 @@
 import test from 'tape';
 
-test('#tape.spec', (t) => {
+test('tape.spec', (t) => {
+
   t.test('Test 1 should fail', (t) => {
 
-    /*
-     * Run the tests and see that assertions can pass even
-     * if the entire test fails
-     */
+    const actual = typeof Array.prototype.map;
+    const expected = 'object';
 
-    const actual = 5;
-    const expected = 5;
-
-    // t.pass(); This would generate a passing assertion
-    t.comment('Assertion passes');
-    t.equals(actual, expected);
-    // t.fail(); This would generate a failing assertion
-    t.comment('Assertion fails');
-    t.notEqual(actual, expected);
+    t.equals(actual, expected, 'Array.prototype.map is an object');
     // t.end explicitly tells the test to end if you don't want to use plan
     t.end();
   });
+
+  t.test('Test 2 should pass', (t) => {
+    const actual = typeof Array.prototype.map;
+    const expected = 'function';
+
+    t.equals(actual, expected, 'Array.prototype.map is a function');
+    t.end();
+  });
+
   t.end();
 });
