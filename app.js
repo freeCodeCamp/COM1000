e6ff4a9d56527b63c946a3d2c071b7bf5008e33c
Ben McMahon <benmcmahon100@gmail.com>
2016-01-23 22:21:26 +0000

    Replaced all parseInts with parseFloat

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ app.post('/export', function(req, res, next) {
       });
       keys.forEach(function(key){
         if(key === "challengeType"){
-          newData[key] = parseInt(challenge[key]);
+          newData[key] = parseFloat(challenge[key]);
         }
         else {
           if(typeof challenge[key] !== 'object'){
@@ -125,7 +125,7 @@ app.post('/export', function(req, res, next) {
       }
     });
     fileData = newFileData;
-    fileData.order = parseInt(fileData.order,10);
+    fileData.order = parseFloat(fileData.order,10);
     fs.writeFile(config.fccPath + file,
       JSON.stringify(fileData, null, 2) + "\n",
       function(err) {
